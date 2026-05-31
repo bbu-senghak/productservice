4d590b8455857348d38ddfffa6c8109efddc761c
bbu-senghak <sorn_senghak@pp.bbu.edu.kh>
2026-05-31 13:56:57 +0700

    feat(product): resolve merge conflict and add product unit

--- src/main/java/kh/com/bbu/productservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/kh/com/bbu/productservice/exceptions/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
+<<<<<<< HEAD
     public ResponseEntity<MessageResponse> handleApiException(ApiException e) {
 
         // Assuming MessageResponse has a constructor or setter for the message
@@ -21,3 +22,22 @@ public class GlobalExceptionHandler {
                 .body(response);
     }
 }
+=======
+    public ResponseEntity<MessageResponse<?>> handleApiException(ApiException e) {
+        HttpStatus status = switch (e.getError()) {
+            case "404" -> HttpStatus.NOT_FOUND;
+            case "400" -> HttpStatus.BAD_REQUEST;
+            default -> HttpStatus.BAD_REQUEST;
+        };
+
+        MessageResponse<?> response = new MessageResponse<>();
+        response.setMessage(e.getMessage());
+        response.setErrorCode(e.getError());
+        response.setSuccess(false);
+
+        // Return the response body along with an HTTP status code
+        // You can change BAD_REQUEST to whatever status your ApiException dictates
+        return ResponseEntity.status(status).body(response);
+    }
+}
+>>>>>>> 417d379 (feat(product): resolve merge conflict and add product unit)
