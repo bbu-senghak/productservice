ddd53f20244da582b855292f85f7d87be970e389
bbu-senghak <sorn_senghak@pp.bbu.edu.kh>
2026-05-31 14:09:09 +0700

    fix(product): resolve exception handling and category lookup

--- src/main/java/kh/com/bbu/productservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/kh/com/bbu/productservice/exceptions/GlobalExceptionHandler.java
@@ -9,20 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-<<<<<<< HEAD
-    public ResponseEntity<MessageResponse> handleApiException(ApiException e) {
-
-        // Assuming MessageResponse has a constructor or setter for the message
-        MessageResponse response = new MessageResponse(e.getMessage());
-
-        // Return the response body along with an HTTP status code
-        // You can change BAD_REQUEST to whatever status your ApiException dictates
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(response);
-    }
-}
-=======
     public ResponseEntity<MessageResponse<?>> handleApiException(ApiException e) {
         HttpStatus status = switch (e.getError()) {
             case "404" -> HttpStatus.NOT_FOUND;
@@ -40,4 +26,3 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(status).body(response);
     }
 }
->>>>>>> 417d379 (feat(product): resolve merge conflict and add product unit)
